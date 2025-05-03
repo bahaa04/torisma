ed8aa3ef26a2270dff0adcb871d26ea57c902246
Miki-co <melekallal693@gmail.com>
2025-05-03 22:43:07 +0100

    malek's work part 1

---
+++ torisma/Front_End/react-vite/src/pages/localisation.jsx
@@ -0,0 +1,90 @@
+
+import React, { useState } from 'react';
+import '../components/styles/Localisation.css';
+import PropertyGallery from '../components/PropertyGallery';
+import PropertyRating from '../components/PropertyRating';
+import BookingForm from '../components/BookingForm';
+import LocationMap from '../components/LocationMap';
+import PropertyDescription from '../components/PropertyDescription';
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar'; 
+import BookingConfirmationForm from '../components/BookingConfirmationForm';
+import PaymentConfirmation from '../components/PaymentConfirmation';
+import SuccessMessage from '../components/SuccessMessage';  
+import Erreur from '../components/Erreur';
+function Localisation() {
+    const [showConfirmation, setShowConfirmation] = useState(false);
+    const [showPayment, setShowPayment] = useState(false);
+    const [showSuccess, setShowSuccess] = useState(false);
+    const [showError, setShowError] = useState(false);
+
+  return (
+    <div>
+      <NavBar />
+      <PropertyGallery />
+      
+      <div className="localisation-container"> {/* Parent container */}
+      
+      <div className="right-column"> <>
+      <PropertyRating />
+    <LocationMap  />    
+    <PropertyDescription /> 
+      
+    </>
+    </div>
+
+
+   
+
+      
+<div className="left-column"> {/* Container for the left column */}
+    
+    <>
+      <BookingForm onReserve={() => setShowConfirmation(true)} />
+      {showConfirmation && (
+      <BookingConfirmationForm
+      onClose={() => setShowConfirmation(false)}
+      onNext={() => {
+      setShowConfirmation(false);
+      setShowPayment(true);
+      }}
+      />
+      )}
+    </>
+    </div>
+    </div>
+
+    {showPayment && (
+  <PaymentConfirmation
+    onBack={() => setShowPayment(false)}
+    onConfirm={() => {
+      setShowPayment(false);
+      setShowSuccess(true);
+    }}
+    onError={() => {
+        setShowPayment(false);
+        setShowError(true);
+      }}
+    
+  />
+)}
+{showSuccess && (
+  <SuccessMessage onClose={() => setShowSuccess(false)} />
+   )}
+    {showError && (
+   <Erreur
+    onRetry={() => {
+      setShowError(false);
+      setShowPayment(true);
+    }}
+    onCancel={() => {
+      setShowError(false);
+    }}
+    />
+    )}
+    <Footer />
+    </div>
+  )
+}
+
+export default Localisation;
---
+++ torisma/Front_End/react-vite/src/pages/moi.jsx
@@ -0,0 +1,18 @@
+import React from "react";  
+import NavBarprofile from '../components/NavBarprofile'; 
+
+import ProfileInformations from "../components/ProfileInformations";
+import Footer from "../components/Footer";
+
+
+
+function Moi() {
+  return (
+    <div>
+      <NavBarprofile />
+      <ProfileInformations />
+      <Footer />
+    </div>
+  );
+}
+export default Moi;
